3ccef66751bf52e3236348bad2d99ae3a444fb00
Kyrylo <kirill.reznichenkoo@gmail.com>
2024-07-20 16:18:07 +0300

    I added auto-creation for the database/database.sqlite file

--- docker-compose/php/entrypoint.sh
+++ docker-compose/php/entrypoint.sh
@@ -6,6 +6,10 @@ cd /var/www
 
 composer install
 
+if [ ! -e "./app/database/database.sqlite" ]; then
+    touch database/database.sqlite
+fi
+
 if [ ! -e "./.env" ]; then
     cp .env.example .env
     php artisan key:generate
